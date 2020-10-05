f4bb8fedfc5c1e838843bf83b427ee3ab172fbf9
JimvanHemert <63447575+JimvanHemert@users.noreply.github.com>
2020-10-05 13:44:33 +0200

    add visualisation folder

---
+++ Trainer/visualization.py
@@ -0,0 +1,47 @@
+import json
+import pandas as pd
+from flask import Flask, json, request, Response
+import os
+
+#from resources import predictor
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/prediction/<model>', methods=['POST'])
+def forest_fire_prediction(model):
+
+    if model == "SVM":
+        content = request.get_json()
+        df = pd.read_json(json.dumps(content), orient='records') 
+        # orient = Indication of expected JSON string format
+        # E.g. records is like this: [{column -> value}, ... , {column -> value}]
+              
+        # Check if model repo exists on server
+        model_repo = os.environ['MODEL_REPO']
+
+        if model_repo:
+            file_path = os.path.join(model_repo, "model.sav")
+            model = load_model(file_path)
+
+            # Model returns list of predictions
+            result = model.predict(df)
+
+            # Transform list into dict          
+            result_dict = { i : result[i] for i in range(0, len(result) ) }
+
+            # Return prediction result as JSON
+            return json.dumps(result_dict, sort_keys=False, indent=4)
+
+    elif model == "test":
+        # Hardcoded test (source: PlotResults van ML assignment)
+        result = [0.86611321, 0.43659247, 0.85455582, 1.95275274, 0.89050911]
+        result_dict = { i : result[i] for i in range(0, len(result) ) }
+        return json.dumps(result_dict, sort_keys=False, indent=4)
+
+    else:
+        return json.dumps({'message': 'No model found.'}, sort_keys=False, indent=4)
+
+
+app.run(host='0.0.0.0', port=5000)
