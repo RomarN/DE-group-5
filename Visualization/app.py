c67dc8e965ff8301c2a44c4050297ee3e6c99d0d
JimvanHemert <63447575+JimvanHemert@users.noreply.github.com>
2020-10-06 10:12:38 +0200

    Visualize

--- Visualization/app.py
+++ Visualization/app.py
@@ -1,26 +1,26 @@
 import pandas as pd
 from flask import Flask, json, request, Response
-
-from resources import predictor
+import requests
+import base64
+from io import BytesIO
+from matplotlib.figure import Figure
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+@app.route("/")
+def visualize():
+    # https://matplotlib.org/3.3.2/faq/howto_faq.html#how-to-use-matplotlib-in-a-web-application-server
+    # Load necessary data
+    preproccesed_data = os.environ["TRAIN_DB_API"]
+    r = requests.get(preproccesed_data)
+    j = r.json()
+    df_preproccesed_data = pd.DataFrame.from_dict(j)
 
-@app.route('/prediction-cp/<model>', methods=['POST'])
-def predict_perf(model):
-    content = request.get_json()
-    df = pd.read_json(json.dumps(content), orient='records')
-    if model == "mlp":
-        js = predictor.predict(df)
-        resp = Response(js, status=200, mimetype='application/json')
-        resp.headers['Access-Control-Allow-Origin'] = '*'
-        resp.headers['Access-Control-Allow-Methods'] = 'POST'
-        resp.headers['Access-Control-Max-Age'] = '1000'
-        return resp
-    else:
-        return json.dumps({'message': 'the given model is not supported dropped'},
-                          sort_keys=False, indent=4), 400
+    predicted_data = os.environ["PREDICT_DB_API"]
+
+    # Convert to important data
+
 
 
 app.run(host='0.0.0.0', port=5000)
--- Visualization/resources/predictor.py
+++ Visualization/resources/predictor.py
@@ -1,21 +1,20 @@
-import json
-import os
+import base64
+from io import BytesIO
+from matplotlib.figure import Figure
 
-from keras.models import load_model
+# Make plot
+def visualize(original_values, predicted_values):
+    # Generate the figure **without using pyplot**.
+    fig = Figure()
+    ax = fig.subplots()
+    ax.plot(original_values)
+    ax.plot(predicted_values)
 
+    # Save it to a temporary buffer.
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
 
-# make prediction
-def predict(dataset):
-    model_repo = os.environ['MODEL_REPO']
-    if model_repo:
-        file_path = os.path.join(model_repo, "model.h5")
-        model = load_model(file_path)
-        val_set2 = dataset.copy()
-        result = model.predict(dataset)
-        y_classes = result.argmax(axis=-1)
-        val_set2['class'] = y_classes.tolist()
-        dic = val_set2.to_dict(orient='records')
-        return json.dumps(dic, indent=4, sort_keys=False)
-    else:
-        return json.dumps({'message': 'A model cannot be found.'},
-                          sort_keys=False, indent=4)
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return f"<img src='data:image/png;base64,{data}'/>"
+
