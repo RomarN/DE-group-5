d5edc9ee921826f00195a6cd1812a640b81f3226
RomarStudent <63447585+RomarStudent@users.noreply.github.com>
2020-10-29 16:50:40 +0100

    Update preprocess_pipeline.py

--- Assignment 2/Pipelines/Preprocess/preprocess_pipeline.py
+++ Assignment 2/Pipelines/Preprocess/preprocess_pipeline.py
@@ -98,9 +98,9 @@ def run(argv=None, save_main_session=True):
 
     # The pipeline will be run on exiting the with block.
     with beam.Pipeline(options=pipeline_options) as p:
-        train_dataset, test_dataset = (p | 'Create FileName Object' >> ReadFromText(known_args.input)
-                                       | 'Preprocess' >> beam.Map(preprocess_data, known_args.pid, known_args.mbucket)
-                                       | 'Train_Test_Split' >> beam.Partition(split_dataset, 2, ratio=[8, 2]))
+        processed_data = (p | 'Create FileName Object' >> ReadFromText(known_args.input)
+                                       | 'Preprocess' >> beam.Map(preprocess_data, known_args.pid, known_args.mbucket))
+        train_dataset, test_dataset = (processed_data | 'Train_Test_Split' >> beam.Partition(split_dataset, 2, ratio=[8, 2]))
         train_dataset | 'Write' >> WriteToText(known_args.output, file_name_suffix=".csv")
         test_dataset | 'Write_test' >> WriteToText(known_args.output + "TEST", file_name_suffix=".csv")
 
