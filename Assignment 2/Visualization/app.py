66cc4bb56861545e722a5b1d0dade5188ec268dd
RomarStudent <63447585+RomarStudent@users.noreply.github.com>
2020-10-30 14:38:09 +0100

    Update app.py

--- Assignment 2/Visualization/app.py
+++ Assignment 2/Visualization/app.py
@@ -7,6 +7,8 @@ import pandas as pd
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 from mlxtend.plotting import plot_confusion_matrix
+from google.cloud import storage
+import pickle
 
 app = Flask(__name__, static_folder="templates")
 app.config["DEBUG"] = True
@@ -14,10 +16,16 @@ app.config["DEBUG"] = True
 
 @app.route('/visualize')
 def index():
-
+    project_id = 'weighty-flag-288919'
+    bucket_name = 'delabs2020'
+    client = storage.Client(project=project_id)
+    bucket = client.get_bucket(bucket_name)
+    blob = bucket.blob('results/predictions-00000-of-00001.csv')
+    blob.download_to_filename('download.csv')
+    csv_file = pickle.load(open('download.csv', 'rb'))
 
     # Load necessary data
-    results = pd.read_csv('results_predictions-00000-of-00001.csv', header=None, names=['predicted', 'actual'])
+    results = pd.read_csv(csv_file, header=None, names=['predicted', 'actual'])
 
     # Generate the figure **without using pyplot**.
     cm = confusion_matrix(results['actual'], results['predicted'])
