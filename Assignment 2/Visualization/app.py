57ab916f6d355641ee07c1970c50cb84ff3b2272
RomarStudent <63447585+RomarStudent@users.noreply.github.com>
2020-10-30 14:27:52 +0100

    visualize

--- Assignment 2/Visualization/app.py
+++ Assignment 2/Visualization/app.py
@@ -1,7 +1,4 @@
-from flask import Flask, json, request, Response, render_template
-import requests
-import os
-import json
+from flask import Flask, render_template
 import base64
 from io import BytesIO
 # from matplotlib.figure import Figure
@@ -22,16 +19,13 @@ def index():
     # Load necessary data
     results = pd.read_csv('results_predictions-00000-of-00001.csv', header=None, names=['predicted', 'actual'])
 
-
     # Generate the figure **without using pyplot**.
-
-
     cm = confusion_matrix(results['actual'], results['predicted'])
     plt.figure()
-    plot_confusion_matrix(cm, figsize=(12, 8), hide_ticks=True, cmap=plt.cm.Blues)
+    plot_confusion_matrix(cm, figsize=(7.5, 5), hide_ticks=True, cmap=plt.cm.Blues)
     plt.title("K Neighbors Model - Confusion Matrix")
     plt.xticks(range(2), ["Heart Not Failed", "Heart Fail"], fontsize=16)
-    plt.yticks(range(2), ["Heart Not Failed", "Heart Fail"], fontsize=16)
+    plt.yticks(range(2), ["Heart Not Failed", "Heart Fail"], fontsize=16, rotation=45)
     # Save it to a temporary buffer.
     buf = BytesIO()
     plt.savefig(buf, format="png")
@@ -41,4 +35,4 @@ def index():
     return render_template('index.html', data=data)
 
 
-app.run(host='0.0.0.0', port=5000)
+app.run(host='127.0.0.1', port=5000)
